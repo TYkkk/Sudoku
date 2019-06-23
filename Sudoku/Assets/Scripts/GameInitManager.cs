558ad1a75f0f9406f26865565b5eda3722def143
Tangyu <1833428194@qq.com>
2019-06-23 22:20:24 +0800

    GameManager

---
+++ Sudoku/Assets/Scripts/GameInitManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitManager
+{
+    public int InitNum = 9;
+
+    public GameObject OneSdTarget;
+
+    #region 单例
+    private static GameInitManager _instance;
+
+    public static GameInitManager Instance {
+        get {
+            if( _instance == null ) {
+                _instance = new GameInitManager( );
+            }
+            return _instance;
+        }
+    }
+
+    private GameInitManager() {
+
+    }
+    #endregion
+
+
+}
